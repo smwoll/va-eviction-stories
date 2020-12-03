36e8df1a7e3fe3b72138d39866388f37b665bea8
Sky <skylandwoll@gmail.com>
2020-12-03 18:41:21 -0500

    setting up scss folders and add normalize

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -35,7 +35,7 @@ const Navbar = class extends React.Component {
   render() {
     return (
       <nav
-        className="navbar is-transparent"
+        className="navbar"
         role="navigation"
         aria-label="main-navigation"
       >
