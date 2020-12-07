1cbf53d5b41f9e8fd4e0a680cc3ee87dd0875860
Sky <skylandwoll@gmail.com>
2020-12-07 12:24:30 -0500

    stories and about div arrange

--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -1,41 +1,37 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { graphql } from 'gatsby'
+import React from "react";
+import PropTypes from "prop-types";
+import { graphql } from "gatsby";
 
-
-
-import Layout from '../components/Layout'
-import Content, { HTMLContent } from '../components/Content'
+import Layout from "../components/Layout";
+import Content, { HTMLContent } from "../components/Content";
 
 export const AboutPageTemplate = ({ title, content, contentComponent }) => {
-  const PageContent = contentComponent || Content
+  const PageContent = contentComponent || Content;
 
   return (
-    <section className="section section--gradient">
+    <section className="section about">
       <div className="container">
-        <div className="columns">
-          <div className="column is-10 is-offset-1">
+        <div className="">
+          <div className="">
             <div className="section">
-              <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
-                {title}
-              </h2>
+              <h1 className="title">{title}</h1>
               <PageContent className="content" content={content} />
             </div>
           </div>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
 AboutPageTemplate.propTypes = {
   title: PropTypes.string.isRequired,
   content: PropTypes.string,
   contentComponent: PropTypes.func,
-}
+};
 
 const AboutPage = ({ data }) => {
-  const { markdownRemark: post } = data
+  const { markdownRemark: post } = data;
 
   return (
     <Layout>
@@ -45,14 +41,14 @@ const AboutPage = ({ data }) => {
         content={post.html}
       />
     </Layout>
-  )
-}
+  );
+};
 
 AboutPage.propTypes = {
   data: PropTypes.object.isRequired,
-}
+};
 
-export default AboutPage
+export default AboutPage;
 
 export const aboutPageQuery = graphql`
   query AboutPage($id: String!) {
@@ -63,4 +59,4 @@ export const aboutPageQuery = graphql`
       }
     }
   }
-`
+`;
