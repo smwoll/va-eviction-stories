f4fbd9243361f3c1f1c36adbba10b63119ea6578
Sky <skylandwoll@gmail.com>
2020-12-21 14:50:28 -0500

    newslist.js using json config

--- src/components/NewsList/NewsList.js
+++ src/components/NewsList/NewsList.js
@@ -1,1 +1,19 @@
+import React from "react";
+import newslistdata from "./newslist.json";
 
+const NewsList = () => (
+  <div className="nav-news">
+    <ul>
+      {newslistdata.newslistitems.map((data, index) => {
+        return (
+          <li key={`content_item_${index}`}>
+            <a href="{data.url}" target="_blank" rel="noopener noreferrer">
+              {data.text}
+            </a>
+          </li>
+        );
+      })}
+    </ul>
+  </div>
+);
+export default NewsList;
